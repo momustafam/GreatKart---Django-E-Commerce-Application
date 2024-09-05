f57c3b85bf2352907b7d775fb3e82c0ac0a2434f
Mohammed Mustafa <mohammedmustafa112025@gmail.com>
2024-09-05 20:22:43 +0300

    Finish media monfigurations and customized users admin panel

--- greatkart/products/admin.py
+++ greatkart/products/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from .models import Category
 
 # Register your models here.
-models = [Category]
 
-for model in models:
-    admin.site.register(model)
+@admin.register(Category)
+class Category(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ['name']}
+    list_display = ['name', 'slug']
--- greatkart/users/admin.py
+++ greatkart/users/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import User
 
 # Register your models here.
 
-models = [User]
-
-for model in models:
-    admin.site.register(model)
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    list_display = ['email', 'username', 'first_name', 'last_name', 'last_login', 'date_joined', 'is_active']
+    list_display_links = ['email', 'username', 'first_name', 'last_name']
+    readonly_fields = ['last_login', 'date_joined']
+    ordering = ['last_login']
+    
+    # For now, not applying horizontal filters or list filters, or customizing fieldsets
+    filter_horizontal = ()  # No many-to-many relationships to filter
+    list_filter = ()  # No sidebar filters to display
+    fieldsets = ()  # Default layout of fields, no custom grouping
